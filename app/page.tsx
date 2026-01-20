6f8e64c0908ae7a1ab6c931cba7c0f541a85dacd
Ahmed-dr-dev <aahmeddraaief@gmail.com>
2026-01-20 23:48:56 +0100

    03

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
           <div className="relative lg:h-[500px] flex items-center justify-center">
             <div className="relative">
               {/* Main Card */}
-          <Image src="/ChatGPT Image Jan 3, 2026, 05_24_31 PM.png" alt="DentAssist Hero" width={500} height={500} className="object-contain" />
+          <Image src="/homepage.png" alt="DentAssist Hero" width={500} height={500} className="object-contain" />
 
 
             </div>
